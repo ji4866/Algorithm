241c852bdfd2391ae048a3f0a4c7f62fae2b5299
ji4866 <65593398+ji4866@users.noreply.github.com>
2024-12-31 01:37:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -2,7 +2,7 @@ def solution(price):
     answer = 0
     if price>=500_000:
         return int(price - price*0.2)
-    elif price>=300_000 and price<500000:
+    elif price>=300_000:
         return int(price - price*0.1)
     elif price>= 100_000:
         return int(price - price*0.05)
